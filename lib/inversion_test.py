cbfcf3d42094d47892a7c887e5ee5e86e7fe7454
P S Mamkani <psmamkani@protonmail.com>
2022-10-06 11:07:18 -0700

    test masked pseudo-inverse recon

--- lib/inversion_test.py
+++ lib/inversion_test.py
@@ -11,6 +11,21 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
+# utility plottig
+def plot_multiple_images(im_arr, nrows=1, ncols=2, fig_size=(12, 6), col_map="plasma", show_cbar=True):
+    sz = len(im_arr)
+    if (nrows * ncols != sz): nrows, ncols = (1, sz) 
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
+    fig.set_size_inches(fig_size)
+    plots, ims, cax = (im_arr, [None] * sz, [None] * sz)
+    for ind in range(sz):
+        ims[ind] = axs[ind].imshow(plots[ind], cmap=cm.get_cmap(col_map))
+        divider = make_axes_locatable(axs[ind])
+        if show_cbar:
+            cax[ind] = divider.append_axes("right", size="5%", pad=0.05)
+            plt.colorbar(ims[ind], cax=cax[ind])
+    plt.show()
+
 
 # test instationaion of an inverse2D object using a 20x20x20x20 projectogram: 
 # rank, training, errors
@@ -162,6 +177,7 @@ def train_pacman_20(num_angs=20, plot_proj=True, plot_recon=True):
     # RMSE: 0.0307 MAE: 0.0202
     # 15 angles: RMSE: 0.0722 MAE: 0.0563
 
+
 # pseudoinverse sinpix and projectogram test performance
 def pseudoinv_instance_20(num_angs=20, plot_sinpix=False, plot_projgram=True):
     proj_angs = np.linspace(start=-90, stop=90, num=num_angs, endpoint=False)
@@ -237,7 +253,6 @@ def pseudoinv_instance_20(num_angs=20, plot_sinpix=False, plot_projgram=True):
         plt.show()
 
 
-
 # projection and inversion of a 20x20 pacman using pseudo-inverse
 def pseudoinv_pacman_20(num_angs=20, plot_proj=True, plot_recon=True):
     pacman_20 = pacman_mask(20, (9.5,9.5), 7, direc=0, ang=60)
@@ -310,9 +325,141 @@ def pseudoinv_pacman_20(num_angs=20, plot_proj=True, plot_recon=True):
     return
 
 
+# projection and inversion of a 20x20 pacman using masked pseudo-inverse (mask applied to recon image)
+def pseudoinv_pacman_masked_20(num_angs=5, plot_proj=True, plot_recon=True):
+    pacman_20 = pacman_mask(20, (9.5,9.5), 7, direc=0, ang=60)
+
+    proj_angs = np.linspace(start=-90, stop=90, num=num_angs, endpoint=False)
+    im_mat_sh = (20, 20)
+    det_len = 20
+    pix_ph_sz = 1.0
+    det_elm_ph_sz = 1.0
+    det_ph_offs = 0.0
+
+    proj_20 = Projection2D(im_mat_sh=im_mat_sh, 
+                            det_len=det_len, 
+                            pix_ph_sz=pix_ph_sz, 
+                            det_elm_ph_sz=det_elm_ph_sz, 
+                            det_ph_offs=det_ph_offs, 
+                            proj_angs=proj_angs)
+
+    sinpix_20 = proj_20.build_single_pixel_mats_dense()
+    projectogram_20 = proj_20.gen_single_pixel_projs(sinpix_20)
+    proj_mat = proj_20.single_mat_all_ang_proj(pacman_20, is_sparse=False)
+
+    if plot_proj:
+        fig, axs = plt.subplots(nrows=1, ncols=2)
+        fig.set_size_inches(12, 6)
+        im0 = axs[0].imshow(pacman_20, cmap=cm.get_cmap("plasma"))
+        im1 = axs[1].imshow(proj_mat, cmap=cm.get_cmap("plasma"))
+        plt.show()
+
+    inverse_20 = Inversion2D(projectogram = projectogram_20,
+                                single_pixel_mats = sinpix_20,
+                                im_mat_sh=im_mat_sh, 
+                                det_len=det_len,
+                                proj_angs=proj_angs, 
+                                pix_ph_sz=pix_ph_sz, 
+                                det_elm_ph_sz=det_elm_ph_sz, 
+                                det_ph_offs=det_ph_offs)
+
+    recon_20, ps_inv, ps_rank_20 = inverse_20.general_projection_recon_pseudoinv_masked(proj_mat, verbose=False)
+
+    if plot_recon:
+        fig, axs = plt.subplots(nrows=1, ncols=2)
+        fig.set_size_inches(12, 6)
+
+        im0 = axs[0].imshow(pacman_20, cmap=cm.get_cmap("plasma"))
+        axs[0].set_title("pacman_20: Original")
+        divider0 = make_axes_locatable(axs[0])
+        cax0 = divider0.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im0, cax=cax0)
+
+        im1 = axs[1].imshow(recon_20, cmap=cm.get_cmap("plasma"))
+        axs[1].set_title(f"pacman_20: Reconstructed with rank {ps_rank_20}")
+        divider1 = make_axes_locatable(axs[1])
+        cax1 = divider1.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im1, cax=cax1)
+        plt.show()
+
+    # calculate phantom-vs-reconstruction errors:
+    rmse = mean_squared_error(pacman_20, recon_20, squared=False)
+    mae = mean_absolute_error(pacman_20, recon_20)
+    print(f"pacman_20 masked reconstruction errors:\n RMSE: {rmse:.4f} MAE: {mae:.4f}")
+    # num angs 4: RMSE: 0.0982 MAE: 0.0642
+
+    return
+
+
+
+# projection and inversion of a 20x20 pacman using masked pseudo-inverse (applied to projectogram)
+def pseudoinv_pacman_masked_20_2(num_angs=5, plot_proj=True, plot_recon=True):
+    pacman_20 = pacman_mask(20, (9.5,9.5), 7, direc=0, ang=60)
+
+    proj_angs = np.linspace(start=-90, stop=90, num=num_angs, endpoint=False)
+    im_mat_sh = (20, 20)
+    det_len = 20
+    pix_ph_sz = 1.0
+    det_elm_ph_sz = 1.0
+    det_ph_offs = 0.0
+
+    proj_20 = Projection2D(im_mat_sh=im_mat_sh, 
+                            det_len=det_len, 
+                            pix_ph_sz=pix_ph_sz, 
+                            det_elm_ph_sz=det_elm_ph_sz, 
+                            det_ph_offs=det_ph_offs, 
+                            proj_angs=proj_angs)
+
+    sinpix_20 = proj_20.build_single_pixel_mats_dense()
+    projectogram_20 = proj_20.gen_single_pixel_projs(sinpix_20)
+    proj_mat = proj_20.single_mat_all_ang_proj(pacman_20, is_sparse=False)
+
+    if plot_proj:
+        fig, axs = plt.subplots(nrows=1, ncols=2)
+        fig.set_size_inches(12, 6)
+        im0 = axs[0].imshow(pacman_20, cmap=cm.get_cmap("plasma"))
+        im1 = axs[1].imshow(proj_mat, cmap=cm.get_cmap("plasma"))
+        plt.show()
+
+    inverse_20 = Inversion2D(projectogram = projectogram_20,
+                                single_pixel_mats = sinpix_20,
+                                im_mat_sh=im_mat_sh, 
+                                det_len=det_len,
+                                proj_angs=proj_angs, 
+                                pix_ph_sz=pix_ph_sz, 
+                                det_elm_ph_sz=det_elm_ph_sz, 
+                                det_ph_offs=det_ph_offs)
+
+    recon_20, ps_inv, ps_rank_20 = inverse_20.general_projection_recon_pseudoinv_masked_2(proj_mat, verbose=False)
+
+    if plot_recon:
+        fig, axs = plt.subplots(nrows=1, ncols=2)
+        fig.set_size_inches(12, 6)
+
+        im0 = axs[0].imshow(pacman_20, cmap=cm.get_cmap("plasma"))
+        axs[0].set_title("pacman_20: Original")
+        divider0 = make_axes_locatable(axs[0])
+        cax0 = divider0.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im0, cax=cax0)
+
+        im1 = axs[1].imshow(recon_20, cmap=cm.get_cmap("plasma"))
+        axs[1].set_title(f"pacman_20: Reconstructed with rank {ps_rank_20}")
+        divider1 = make_axes_locatable(axs[1])
+        cax1 = divider1.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im1, cax=cax1)
+        plt.show()
+
+    # calculate phantom-vs-reconstruction errors:
+    rmse = mean_squared_error(pacman_20, recon_20, squared=False)
+    mae = mean_absolute_error(pacman_20, recon_20)
+    print(f"pacman_20 masked reconstruction errors:\n RMSE: {rmse:.4f} MAE: {mae:.4f}")
+    
+    return
+
 # train_instance_20(plot_sinpix=True, plot_projgram=True)
 # train_pacman_20(num_angs=20, plot_proj=False, plot_recon=True)
-pseudoinv_instance_20(num_angs=10, plot_sinpix=True, plot_projgram=True)
+# pseudoinv_instance_20(num_angs=10, plot_sinpix=True, plot_projgram=True)
 # pseudoinv_instance_20(num_angs=4, plot_sinpix=True, plot_projgram=True)
 
-# pseudoinv_pacman_20(num_angs=10, plot_proj=True, plot_recon=True)
+pseudoinv_pacman_20(num_angs=4, plot_proj=False, plot_recon=True)
+pseudoinv_pacman_masked_20(num_angs=4, plot_proj=False, plot_recon=True)
